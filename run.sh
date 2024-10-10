5fc3b6d2bfe22c259b6d38adcaf4ceee7b4083d0
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-10-10 16:55:21 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 INFERENCE_RESULTS_REPO_OWNER=${INFERENCE_RESULTS_REPO_OWNER:-mlcommons}
+if [ ! -e docs ]; then
+    git clone https://github.com/GATEOverflow/inference_results_visualization_template.git docs
+    test $? -eq 0 || exit $?
+fi
+cp docs/docinit.sh .
 INFERENCE_RESULTS_VERSION=v4.1 INFERENCE_RESULTS_REPO_OWNER=${INFERENCE_RESULTS_REPO_OWNER} INFERENCE_RESULTS_REPO_BRANCH=main INFERENCE_RESULTS_REPO_NAME=inference_results_v4.1 bash docinit.sh
